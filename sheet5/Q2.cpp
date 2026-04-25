d4a42f2768a4553d57ef7160b7c95f86136ba3fc
Hamed <106395391+aboremsh@users.noreply.github.com>
2026-04-26 00:05:44 +0300

    Update Q2.cpp

--- sheet5/Q2.cpp
+++ sheet5/Q2.cpp
@@ -1,105 +1,126 @@
 #include <iostream>
 using namespace std;
-/*
-
-*/
 
 class Matrix
 {
-  float M[20][20];
+  float M[5][5];
   int n;
-
 public:
-  void read()
+  void read ()
   {
     cout << "Enter n: ";
     cin >> n;
-    cout << "Enter M[" << n << "][" << n - 1 << "]\n";
+
+    cout << "Enter values of M[" << n << "][" << n - 1 << "]:\n";
     for (int i = 0; i < n; i++)
       for (int j = 0; j < n - 1; j++)
         cin >> M[i][j];
   }
-  float sumForGivenIndex(int a)
+  float sumGivenIndex (int a)
   {
     float sum = 0;
-    for (int i = 0; i <= n - 2; i++)
-      sum += M[a][i];
+
+    for (int j = 0; j < n - 1; j++)
+    {
+      sum += M[a][j];
+    }
     return sum;
   }
-  void setLastCol()
+  void set()
   {
     for (int i = 0; i < n; i++)
-      M[i][n - 1] = sumForGivenIndex(i);
+    {
+      M[i][n - 1] = sumGivenIndex(i);
+    }
   }
   void display()
   {
-    cout << "M[" << n << "]:\n";
+    cout << "n: " << n << endl;
+
+    cout << "M[" << n << "][" << n << "]\n";
     for (int i = 0; i < n; i++)
+    {
       for (int j = 0; j < n; j++)
       {
-        if (j + 1 == n)
+        if(j + 1 == n)
         {
-          cout << M[i][j] << endl;
+          cout << M[i][j] << "\n";
           break;
         }
-        cout << M[i][j] << " ";
+        cout << M[i][j] << "\t";
       }
+    }
   }
-  Matrix operator++()
+  // prefix
+  Matrix operator-- ()
   {
     for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
-        M[i][j]++;
+        M[i][j]--;
+
     return *this;
   }
-  // postfix ++
-  Matrix operator++(int)
+  // postfix
+  Matrix operator-- (int)
   {
     Matrix temp = *this;
+
     for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
-        M[i][j]++;
+        M[i][j]--;
+
     return temp;
   }
-  Matrix operator*(int a)
+  bool operator== (Matrix obj)
   {
-    Matrix mat = *this;
     for (int i = 0; i < n; i++)
-      for (int j = 0; j < n; j++)
-        mat.M[i][j] *= a;
-    mat.setLastCol();
-    return mat;
+    {
+      if(M[i][n - 1] != obj.M[i][n - 1])
+        return false;
+    }
+    return true;
   }
-  bool operator==(Matrix a)
+  bool operator> (Matrix obj)
   {
     for (int i = 0; i < n; i++)
-      if (M[i][n - 1] == a.M[i][n - 1])
+    {
+      if(M[i][n - 1] > obj.M[i][n - 1])
         return true;
+    }
     return false;
   }
-  bool operator>(Matrix a)
+
+  Matrix operator* (int a)
   {
-    bool flag = false;
+    Matrix temp = *this;
+  
     for (int i = 0; i < n; i++)
-      if (M[i][n - 1] > a.M[i][n - 1])
-        flag = true;
-    return flag;
+    {
+      for (int j = 0; j < n; j++)
+      {
+        temp.M[i][j] *= a;
+      }
+    }
+  
+    temp.set();
+    return temp;
   }
 };
+
 
 int main()
 {
   Matrix x, y, z;
 
   x.read();
-  x.setLastCol();
+  x.set();
   x.display();
 
   y.read();
-  y.setLastCol();
+  y.set();
   y.display();
 
-  z = ++x;
+  // z = --x;
 
   if (x == y)
     cout << "These objects are equal\n";
@@ -108,12 +129,14 @@ int main()
 
   if (x > y)
   {
-    cout << "The max object is:\n";
+    cout << "The max object is X:\n";
     x.display();
   }
   else
   {
-    cout << "The max object is:\n";
+    cout << "The max object is Y:\n";
     y.display();
   }
 }
+
+
